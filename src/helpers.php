ad2c36e68b9c5a1dc08266c4da68b6829d1cbcae
Elf Sundae <elf.sundae@gmail.com>
2016-09-25 17:39:16 +0800

    Add code comment

--- src/helpers.php
+++ src/helpers.php
@@ -1,6 +1,12 @@
 <?php
 
 if (! function_exists('bearychat')) {
+    /**
+     * Get the BearyChat client instance.
+     *
+     * @param  string  $name
+     * @return \ElfSundae\BearyChat\Client
+     */
     function bearychat($name = null)
     {
         return app('bearychat')->client($name);
