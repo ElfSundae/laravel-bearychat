915fbf070fe7bbd21ceba155a97f7e0c9ea3c2a3
Elf Sundae <elf.sundae@gmail.com>
2016-09-25 20:19:54 +0800

    Add more tests

--- tests/ClientManagerTest.php
+++ tests/ClientManagerTest.php
@@ -3,6 +3,7 @@
 namespace ElfSundae\BearyChat\Laravel\Test;
 
 use ElfSundae\BearyChat\Client;
+use ElfSundae\BearyChat\Message;
 use ElfSundae\BearyChat\Laravel\ClientManager;
 use ElfSundae\BearyChat\Laravel\ServiceProvider;
 use Orchestra\Testbench\TestCase;
@@ -19,9 +20,47 @@ class ClientManagerTest extends TestCase
         $this->assertInstanceOf(Client::class, $this->getManager()->client());
     }
 
+    public function testDynamicallyCall()
+    {
+        $this->assertInstanceOf(Message::class, $this->getManager()->text('foo'));
+    }
+
+    public function testResolvedDefaultClient()
+    {
+        $this->app['config']->set('bearychat.default', 'foo');
+
+        $this->assertSame($this->getClient(), $this->getClient('foo'));
+    }
+
+    public function testClientConfig()
+    {
+        $this->app['config']->set('bearychat', [
+            'default' => 'foo',
+            'clients' => [
+                'foo' => [
+                    'webhook' => 'fake.endpoint',
+                    'message_defaults' => [
+                        'user' => 'elf',
+                    ],
+                ],
+            ],
+        ]);
+
+        $client = $this->getClient();
+
+        $this->assertSame('fake.endpoint', $client->getWebhook());
+
+        $this->assertSame('elf', $client->getMessageDefaults('user'));
+    }
+
     protected function getManager()
     {
         return $this->app->make('bearychat');
+    }
+
+    protected function getClient($name = null)
+    {
+        return $this->getManager()->client($name);
     }
 
     /**
