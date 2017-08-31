fe9f39d76430a45e3525bac731b3052c5e0d30f2
Elf Sundae <elf.sundae@gmail.com>
2017-08-31 20:00:38 +0800

    Add clients_defaults config

--- src/config/config.php
+++ src/config/config.php
@@ -16,35 +16,39 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Clients Defaults
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify some defaults for all clients.
+    |
+    | Supported keys: 'webhook', 'message_defaults'.
+    |
+    | All possible keys for 'message_defaults' are listed in
+    | `ElfSundae\BearyChat\MessageDefaults`:
+    | "channel", "user", "markdown" (boolean), "notification", "attachment_color".
+    |
+    */
+
+    'clients_defaults' => [
+        // 'webhook' => 'https://hook.bearychat.com/=',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
     | BearyChat Clients
     |--------------------------------------------------------------------------
     |
     | Here are each of the BearyChat clients setup for your application.
     |
-    | Supported keys:
-    |
-    |   'webhook':      The Incoming Webhook URL. You can get it from an Incoming Robot.
-    |                   See https://bearychat.kf5.com/posts/view/26755/
-    |   'message_defaults': Optional message defaults. All keys of message defaults
-    |                       are listed in `ElfSundae\BearyChat\MessageDefaults`.
-    |                       Supported: "channel", "user", "markdown" (boolean),
-    |                       "notification", "attachment_color".
-    |
     */
 
     'clients' => [
-
         'default' => [
-            'webhook' => '',
-            // 'message_defaults' => [
-            //     'attachment_color' => '#f5f5f5',
-            // ],
+            'webhook' => 'https://hook.bearychat.com/=',
+            'message_defaults' => [
+                'channel' => 'all',
+            ],
         ],
-
-        // 'admin' => [
-        //     'webhook' => '',
-        // ],
-
     ],
 
 ];
